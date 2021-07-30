d49c9ee1798c14daaf3ba790c55684696d508129
Riya Savant <rieee2110@gmail.com>
2021-07-30 23:01:06 +0530

    Project Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from "./Components/Header/index";
+import Editor from "./Components/Editor/index";
+import Input from "./Components/Input/index";
+import Output from "./Components/Output/index";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>  
+        <Header />
+        <Editor />
+        <Input />
+        <Output />
+    </>
   );
 }
 
