d9a83cdc695113465abfd76abd967e364e42ea18
Mdsid7 <mdsiddiq.as@gmail.com>
2021-07-31 09:31:21 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from "./Components/Header/index";
 import Editor from "./Components/Editor/index";
 import Input from "./Components/Input/index";
 import Output from "./Components/Output/index";
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [code, setCode] = useState('');
+  const [language, setLanguage] = useState('');
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState({});
+  const [lightTheme, setTheme] = useState(true);
+
+  const handleCode = data => {
+    setCode(data);
+  }
+
+  const handleLanguage = data => {
+    setLanguage(data);
+  }
+
+  const handleInput = data => {
+    setInput(data);
+  }
+
+  const changeTheme = () => {
+    setTheme(!lightTheme);
+  }
+
+  async function handleSubmit() {
+    const response = await axios.post('http://localhost:5000/run', {language: language, code: code, input: input});
+    const data = response.json();
+  }
+
   return (
-    <>  
-        <Header />
-        <Editor />
-        <Input />
-        <Output />
-    </>
+    <div className={`content ${lightTheme ? 'light' : 'dark'}`}>  
+        <Header lightTheme={lightTheme} changeTheme={changeTheme}/>
+       
+       
+            <Editor />
+            <Input />
+            <Output/>
+       
+       
+    </div>
   );
 }
 
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
+import dark from "../../assets/moon.png";
+import light from "../../assets/sun.png";
 import "./index.css";
 
-export default function Header() {
+
+
+export default function Header({lightTheme, changeTheme}) {
+
+
     return(
-        <div>Code For Header</div>
-    )
+        <header className="header">
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                <div className="flex">
+                    <div style={{fontSize: '30px', marginRight: '20px'}}>Code Editor</div>
+                    <div onClick={changeTheme} style={{cursor: 'pointer', display: 'flex', alignItems: 'center'}}><img src={lightTheme ? dark : light} alt="Theme Icon" height="18px" width="18px"/></div>
+                </div> 
+
+                <div className="flex">
+                    <button style={{padding: '7px', boxShadow: '1px 1px 4px 1px #D14D5D', border: 'none', background: '#D14D5D', marginRight: '10px', color: 'white'}}>Reset</button>
+               
+                    <select style={{padding : '7px', boxShadow: '1px 1px 4px 1px #C4C4C4' ,marginRight: '10px', minWidth: '25vh' , background: '#C4C4C4'}} name="language" className=""> 
+                       
+                        <option value="javascript">Javascript</option>
+                        <option value="c">C</option>
+                        <option value="cpp">C++</option>
+                        <option value="java">Java</option>
+                        <option value="python">Python</option>
+                    </select>     
+                    
+                    <button className='run' style={{padding: '7px', border: 'none', background: '#2CCACA', color: 'white'}}>Run</button>
+                </div> 
+
+            </div>
+        </header>
+    );
 }
